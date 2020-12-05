0ef6df21e857d77db23e10b301a66400fa982cd8
Bilal Junaidy <bilal.junaidy@gmail.com>
2020-12-05 19:26:27 +0500

    s

--- SeekhouAurSikhaou/learn/models.py
+++ SeekhouAurSikhaou/learn/models.py
@@ -1,7 +1,43 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import datetime 
+from django import forms
+
 
 # Create your models here.
 
 class User(AbstractUser):
-    pass
+    TYPE_OPTIONS = [
+        ('STUDENT', 'student'),
+        ('TEACHER', 'teacher'),
+        ('PARENT', 'parent'),
+    ]
+    type = models.CharField(max_length=7, choices=TYPE_OPTIONS, default='STUDENT', blank=False)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name="users")
+
+class Course(models.Model):
+    name = models.CharField(max_length = 150)
+    description = models.TextField()
+    start_date = models.DateField(auto_now = datetime.datetime.now())
+    end_date = models.DateField()
+    # Duration 
+    Status = models.BooleanField(default = True)
+    
+class Section(models.Model):
+    TERM_OPTIONS = [
+        ('FALL','Fall'),
+        ('WINTER','Winter'),
+        ('SUMMER','Summer')
+    ]
+    name = models.CharField(max_length = 150)
+    description = models.TextField()
+    start_date = models.DateField(auto_now = datetime.datetime.now())
+    end_date = models.DateField()
+    # Duration = 
+    status = models.BooleanField(default = True)
+    academic_year = models.PositiveSmallIntegerField()
+    academic_term = models.CharField(max_length=1000, choices=TERM_OPTIONS, default='FALL', blank=False)
+    course = models.ForeignKey('Course', on_delete=models.CASCADE, related_name="sections")
+    teacher = models.ForeignKey('User', on_delete=models.CASCADE, related_name="sections")
+
+
--- SeekhouAurSikhaou/learn/views.py
+++ SeekhouAurSikhaou/learn/views.py
@@ -1,3 +1,93 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.forms import ModelForm
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from django.db import IntegrityError
+
+from .models import User, Course, Section
 
 # Create your views here.
+
+@login_required(login_url='login')
+def index(request):
+    class userform(ModelForm):
+        class Meta:
+            model = User
+            fields = ['username','password','first_name','last_name','email','type','parent']
+    
+    class courseform(ModelForm):
+        class Meta:
+            model = Course 
+            fields = '__all__'
+        
+    class sectionform(ModelForm):
+        class Meta:
+            model = Section
+            fields = '__all__'
+    
+    
+    userform = userform()
+    courseform = courseform()
+    sectionform = sectionform()
+
+    # return HttpResponse(f"{request.user.type}, {request.user.first_name}, {type(request.user)},{request.user.id}")
+    return render(request, "learn/index.html", {
+        "userform":userform,
+        "courseform":courseform,
+        "sectionform":sectionform,
+    })
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = authenticate(request, username=email, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "learn/login.html", {
+                "message": "Invalid email and/or password."
+            })
+    else:
+        return render(request, "learn/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "learn/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(email, email, password)
+            user.save()
+        except IntegrityError as e:
+            print(e)
+            return render(request, "learn/register.html", {
+                "message": "Email address already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "learn/register.html")
+        
