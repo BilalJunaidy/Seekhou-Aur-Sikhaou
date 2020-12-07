252e3d421227aa953aa11271655b7e6a8770854b
Bilal Junaidy <bilal.junaidy@gmail.com>
2020-12-07 11:51:19 +0500

    In this commit, I have basically used the multi-table inheritance method of creating the 3 different types of Users. Based on its underlying implementation challenges, I will not be using this in the actual project. But would still like to make this commit so that I know how multi-table inheritance works for extending the User model class

--- SeekhouAurSikhaou/learn/models.py
+++ SeekhouAurSikhaou/learn/models.py
@@ -3,15 +3,14 @@ from django.db import models
 import datetime 
 from django.utils import timezone
 
-
 # Create your models here.
 
 class User(AbstractUser):
-    TYPE_OPTIONS = [
-        ('STUDENT', 'student'),
-        ('TEACHER', 'teacher'),
-        ('PARENT', 'parent'),
-    ]
+    # TYPE_OPTIONS = [
+    #     ('STUDENT', 'student'),
+    #     ('TEACHER', 'teacher'),
+    #     ('PARENT', 'parent'),
+    # ]
     GRADE_OPTIONS = [
         ('GRADE5','Grade 5'),
         ('GRADE6','Grade 6'),
@@ -26,124 +25,133 @@ class User(AbstractUser):
     profile_image = models.ImageField()
     grade_level = models.CharField(max_length=7, choices=GRADE_OPTIONS, blank=False, default='GRADE5')
 
-class Student(User):
-    type = models.CharField(max_length=7, default='STUDENT', blank=False)
-    parent = models.ForeignKey('Parent', on_delete=models.CASCADE, null=True, blank=True, related_name="students")
+    # class Meta:
+    #     abstract = True 
 
-class Parent(User):
+class ParentProfile(User):
     type = models.CharField(max_length=7, default='PARENT', blank=False)
+    class Meta:
+        verbose_name = 'Parent List'
 
-class Teacher(User):
+class StudentProfile(User):
+    type = models.CharField(max_length=7, default='STUDENT', blank=False)
+    parent_field = models.ForeignKey('ParentProfile', on_delete=models.CASCADE, null=False, blank=False, related_name="students")
+    class Meta:
+        verbose_name = 'Student List'
+
+class TeacherProfile(User):
     type = models.CharField(max_length=7, default='TEACHER', blank=False)
+    class Meta:
+        verbose_name = 'Teacher List'
 
 
-class Course(models.Model):
-    name = models.CharField(max_length = 150)
-    code = models.CharField(max_length = 100)
-    description = models.TextField()
-    start_date = models.DateField()
+# class Course(models.Model):
+#     name = models.CharField(max_length = 150)
+#     code = models.CharField(max_length = 100)
+#     description = models.TextField()
+#     start_date = models.DateField()
 
-    # For now I have changed the default value of the datefield to be equal to datetime.datetime.now since I would like the user to define this field 
-    # To come back to this in the continous improvement phase (post-Hackathon)
-    # start_date = models.DateField(auto_now = datetime.datetime.now())
+#     # For now I have changed the default value of the datefield to be equal to datetime.datetime.now since I would like the user to define this field 
+#     # To come back to this in the continous improvement phase (post-Hackathon)
+#     # start_date = models.DateField(auto_now = datetime.datetime.now())
     
-    end_date = models.DateField()
-    # Duration 
-    Status = models.BooleanField(default = True)
-    date_created = models.DateTimeField(default = timezone.now)
+#     end_date = models.DateField()
+#     # Duration 
+#     Status = models.BooleanField(default = True)
+#     date_created = models.DateTimeField(default = timezone.now)
     
-class Section(models.Model):
-    TERM_OPTIONS = [
-        ('FALL','Fall'),
-        ('WINTER','Winter'),
-        ('SUMMER','Summer')
-    ]
-    name = models.CharField(max_length = 150)
-    description = models.TextField()
-    start_date = models.DateField(auto_now = datetime.datetime.now())
-    end_date = models.DateField()
-    # Duration = 
-    status = models.BooleanField(default = True)
-    academic_year = models.PositiveSmallIntegerField()
-    academic_term = models.CharField(max_length=1000, choices=TERM_OPTIONS, default='FALL', blank=False)
-    course = models.ForeignKey('Course', on_delete=models.CASCADE, related_name="sections")
-    teacher = models.ForeignKey('User', on_delete=models.CASCADE, related_name="sections")
-    date_created = models.DateTimeField(default = timezone.now)
+# class Section(models.Model):
+#     TERM_OPTIONS = [
+#         ('FALL','Fall'),
+#         ('WINTER','Winter'),
+#         ('SUMMER','Summer')
+#     ]
+#     name = models.CharField(max_length = 150)
+#     description = models.TextField()
+#     start_date = models.DateField(auto_now = datetime.datetime.now())
+#     end_date = models.DateField()
+#     # Duration = 
+#     status = models.BooleanField(default = True)
+#     academic_year = models.PositiveSmallIntegerField()
+#     academic_term = models.CharField(max_length=6, choices=TERM_OPTIONS, default='FALL', blank=False)
+#     course = models.ForeignKey('Course', on_delete=models.CASCADE, related_name="sections")
+#     teacher = models.ForeignKey('Teacher', on_delete=models.CASCADE, related_name="sections")
+#     date_created = models.DateTimeField(default = timezone.now)
 
 
-class Lecture(models.Model):
-    course = models.ForeignKey('Course', on_delete=models.CASCADE, related_name="lectures")
-    section = models.ForeignKey('Section', on_delete=models.CASCADE, related_name="lectures")
-    number = models.IntegerField()
-    name = models.CharField(max_length = 255)
-    description = models.TextField()
-    lecture_date = models.DateField()
-    start_time = models.TimeField(auto_now=False, auto_now_add=False)
-    end_time = models.TimeField(auto_now=False, auto_now_add=False)
-    date_created = models.DateTimeField(default = timezone.now)
-    teacher = models.ForeignKey('User', on_delete=models.CASCADE, related_name="lectures")
+# class Lecture(models.Model):
+#     course = models.ForeignKey('Course', on_delete=models.CASCADE, related_name="lectures")
+#     section = models.ForeignKey('Section', on_delete=models.CASCADE, related_name="lectures")
+#     number = models.IntegerField()
+#     name = models.CharField(max_length = 255)
+#     description = models.TextField()
+#     lecture_date = models.DateField()
+#     start_time = models.TimeField(auto_now=False, auto_now_add=False)
+#     end_time = models.TimeField(auto_now=False, auto_now_add=False)
+#     date_created = models.DateTimeField(default = timezone.now)
+#     teacher = models.ForeignKey('Teacher', on_delete=models.CASCADE, related_name="lectures")
 
-class Attendance(models.Model):
-    ATTENDANCE_OPTIONS = [
-        ('PRESENT','Present'),
-        ('ABSENT','Absent')
-    ]
-    lecture = models.ForeignKey('Lecture', on_delete=models.CASCADE, related_name="attendances")
+# class Attendance(models.Model):
+#     ATTENDANCE_OPTIONS = [
+#         ('PRESENT','Present'),
+#         ('ABSENT','Absent')
+#     ]
+#     lecture = models.ForeignKey('Lecture', on_delete=models.CASCADE, related_name="attendances")
 
-    # Although initially it does appears as though including a course field is overkill. 
-    # However, I think it will be beneficials for management/teachers to review the attendances over the entire course object vs just over a section.
-    # This might give them an insight into which course(s) students are finding more interesting and which ones they are not, and hence, using 
-    # this information, management/teachers will be able to adjust course and section offerings accordingly. 
-    course = models.ForeignKey('Course', on_delete=models.CASCADE, related_name="attendances")
-    section = models.ForeignKey('Section', on_delete=models.CASCADE, related_name="attendances")
-    status = models.CharField(max_length=7, choices = ATTENDANCE_OPTIONS, default = 'PRESENT')
-    student = models.ManyToManyField('User', blank=False, related_name = "attendances")
-    teacher_comments = models.TextField()
+#     # Although initially it does appears as though including a course field is overkill. 
+#     # However, I think it will be beneficials for management/teachers to review the attendances over the entire course object vs just over a section.
+#     # This might give them an insight into which course(s) students are finding more interesting and which ones they are not, and hence, using 
+#     # this information, management/teachers will be able to adjust course and section offerings accordingly. 
+#     course = models.ForeignKey('Course', on_delete=models.CASCADE, related_name="attendances")
+#     section = models.ForeignKey('Section', on_delete=models.CASCADE, related_name="attendances")
+#     status = models.CharField(max_length=7, choices = ATTENDANCE_OPTIONS, default = 'PRESENT')
+#     student = models.ManyToManyField('Student', blank=False, related_name = "attendances")
+#     teacher_comments = models.TextField()
 
-class Lecture_Note(models.Model):
-    section = models.ForeignKey('Section', on_delete=models.CASCADE, related_name="lecturenotes")
-    teacher = models.ForeignKey('User', on_delete=models.CASCADE, related_name="lecturenotes")
-    title = models.CharField(max_length = 255)
-    notes = models.TextField()
-    date_created = models.DateTimeField(default = timezone.now)
-    files = models.FileField()
+# class Lecture_Note(models.Model):
+#     section = models.ForeignKey('Section', on_delete=models.CASCADE, related_name="lecturenotes")
+#     teacher = models.ForeignKey('Teacher', on_delete=models.CASCADE, related_name="lecturenotes")
+#     title = models.CharField(max_length = 255)
+#     notes = models.TextField()
+#     date_created = models.DateTimeField(default = timezone.now)
+#     files = models.FileField()
 
-class Comment(models.Model):
-    lecture_note = models.ForeignKey('Lecture_Note', on_delete=models.CASCADE, related_name="comments")
-    student = models.ForeignKey('User', on_delete=models.CASCADE, related_name="comments")
-    title = models.CharField(max_length = 255)
-    notes = models.TextField()
-    date_created = models.DateTimeField(default = timezone.now)
+# class Comment(models.Model):
+#     lecture_note = models.ForeignKey('Lecture_Note', on_delete=models.CASCADE, related_name="comments")
+#     student = models.ForeignKey('Student', on_delete=models.CASCADE, related_name="comments")
+#     title = models.CharField(max_length = 255)
+#     comment = models.TextField()
+#     date_created = models.DateTimeField(default = timezone.now)
 
-class Assignment(models.Model):
-    title = models.CharField(max_length = 255)
-    description = models.TextField()
-    course = models.ForeignKey('Course', on_delete=models.CASCADE, related_name="assignments")
-    section = models.ForeignKey('Section', on_delete=models.CASCADE, related_name="assignments")
-    start_date = models.DateField()
-    end_date = models.DateField()
-    status = models.BooleanField(default=True)
-    date_created = models.DateTimeField(default = timezone.now)
-    files = models.FileField()
-    teacher = models.ForeignKey('User', on_delete=models.CASCADE, related_name="assignments")
+# class Assignment(models.Model):
+#     title = models.CharField(max_length = 255)
+#     description = models.TextField()
+#     course = models.ForeignKey('Course', on_delete=models.CASCADE, related_name="assignments")
+#     section = models.ForeignKey('Section', on_delete=models.CASCADE, related_name="assignments")
+#     start_date = models.DateField()
+#     end_date = models.DateField()
+#     status = models.BooleanField(default=True)
+#     date_created = models.DateTimeField(default = timezone.now)
+#     files = models.FileField()
+#     teacher = models.ForeignKey('Teacher', on_delete=models.CASCADE, related_name="assignments")
 
-class Submission(models.Model):
-    assignment = models.ForeignKey('Assignment', on_delete=models.CASCADE, related_name="submissions")
-    student = models.ForeignKey('User', on_delete=models.CASCADE, related_name="submissions")
-    title = models.CharField(max_length = 255)
-    description = models.TextField()
-    date_created = models.DateTimeField(default = timezone.now)
-    files = models.FileField()
+# class Submission(models.Model):
+#     assignment = models.ForeignKey('Assignment', on_delete=models.CASCADE, related_name="submissions")
+#     student = models.ForeignKey('Student', on_delete=models.CASCADE, related_name="submissions")
+#     title = models.CharField(max_length = 255)
+#     description = models.TextField()
+#     date_created = models.DateTimeField(default = timezone.now)
+#     files = models.FileField()
 
-class Mark(models.Model):
-    course = models.ForeignKey('Course', on_delete=models.CASCADE, related_name="marks")
-    section = models.ForeignKey('Section', on_delete=models.CASCADE, related_name="marks")
-    assignment = models.ForeignKey('Assignment', on_delete=models.CASCADE, related_name="marks")
-    student = models.ForeignKey('User', on_delete=models.CASCADE, related_name="marks")
-    # teacher = models.ForeignKey('User', on_delete=models.CASCADE, related_name="marks")
-    date_created = models.DateTimeField(default = timezone.now)
-    marks = models.FloatField() 
-    teacher_comments = models.TextField()
+# class Mark(models.Model):
+#     course = models.ForeignKey('Course', on_delete=models.CASCADE, related_name="marks")
+#     section = models.ForeignKey('Section', on_delete=models.CASCADE, related_name="marks")
+#     assignment = models.ForeignKey('Assignment', on_delete=models.CASCADE, related_name="marks")
+#     student = models.ForeignKey('Student', on_delete=models.CASCADE, related_name="marks")
+#     # teacher = models.ForeignKey('User', on_delete=models.CASCADE, related_name="marks")
+#     date_created = models.DateTimeField(default = timezone.now)
+#     marks = models.FloatField() 
+#     teacher_comments = models.TextField()
 
 
 
--- SeekhouAurSikhaou/learn/views.py
+++ SeekhouAurSikhaou/learn/views.py
@@ -6,78 +6,84 @@ from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 from django.db import IntegrityError
 
-from .models import User, Course, Section, Lecture, Attendance, Lecture_Note, Comment, Assignment, Submission, Mark
+from .models import User 
+
+# from .models import User, Course, Section, Lecture, Attendance, Lecture_Note, Comment, Assignment, Submission, Mark
 
 # Create your views here.
 
 # @login_required(login_url='login')
 def index(request):
-    class userform(ModelForm):
-        class Meta:
-            model = User
-            fields = ['username','password','first_name','last_name','email','type','parent', 'grade_level', 'profile_image', 'is_staff']
-    
-    class courseform(ModelForm):
-        class Meta:
-            model = Course 
-            fields = '__all__'
-        
-    class sectionform(ModelForm):
-        class Meta:
-            model = Section
-            fields = '__all__'
-
-    class lectureform(ModelForm):
-        class Meta:
-            model = Lecture 
-            fields = '__all__'
-    
-    class attendanceform(ModelForm):
-        class Meta:
-            model = Attendance
-            fields = '__all__'
-    
-    class lecturenoteform(ModelForm):
-        class Meta:
-            model = Lecture_Note
-            fields = '__all__'
-    
-    class commentform(ModelForm):
-        class Meta:
-            model = Comment
-            fields = '__all__'
-    
-    class assignmentform(ModelForm):
-        class Meta:
-            model = Assignment
-            fields = '__all__'
-    
-    class submissionform(ModelForm):
-        class Meta:
-            model = Submission
-            fields = '__all__'
-
-    class markform(ModelForm):
-        class Meta:
-            model = Mark
-            fields = '__all__'
-    
-    
-    userform = userform()
-    courseform = courseform()
-    sectionform = sectionform()
-    lectureform = lectureform()
-    attendanceform = attendanceform()
-    lecturenoteform = lecturenoteform()
-    commentform = commentform()
-    assignmentform = assignmentform()
-    submissionform = submissionform()
-    markform = markform() 
-
-
+    a = User.objects.get(pk = request.user.id)
+    b = a.studentprofile.type
+    return HttpResponse(f"{request.user}, {b}")
     # return HttpResponse(f"{request.user.type}, {request.user.first_name}, {type(request.user)},{request.user.id}")
+    # class userform(ModelForm):
+    #     class Meta:
+    #         model = User
+    #         fields = ['username','password','first_name','last_name','email','type','parent', 'grade_level', 'profile_image', 'is_staff']
+    
+    # class courseform(ModelForm):
+    #     class Meta:
+    #         model = Course 
+    #         fields = '__all__'
+        
+    # class sectionform(ModelForm):
+    #     class Meta:
+    #         model = Section
+    #         fields = '__all__'
+
+    # class lectureform(ModelForm):
+    #     class Meta:
+    #         model = Lecture 
+    #         fields = '__all__'
+    
+    # class attendanceform(ModelForm):
+    #     class Meta:
+    #         model = Attendance
+    #         fields = '__all__'
+    
+    # class lecturenoteform(ModelForm):
+    #     class Meta:
+    #         model = Lecture_Note
+    #         fields = '__all__'
+    
+    # class commentform(ModelForm):
+    #     class Meta:
+    #         model = Comment
+    #         fields = '__all__'
+    
+    # class assignmentform(ModelForm):
+    #     class Meta:
+    #         model = Assignment
+    #         fields = '__all__'
+    
+    # class submissionform(ModelForm):
+    #     class Meta:
+    #         model = Submission
+    #         fields = '__all__'
+
+    # class markform(ModelForm):
+    #     class Meta:
+    #         model = Mark
+    #         fields = '__all__'
+    
+    
+    # userform = userform()
+    # courseform = courseform()
+    # sectionform = sectionform()
+    # lectureform = lectureform()
+    # attendanceform = attendanceform()
+    # lecturenoteform = lecturenoteform()
+    # commentform = commentform()
+    # assignmentform = assignmentform()
+    # submissionform = submissionform()
+    # markform = markform() 
+
+
+
     return render(request, "learn/index.html", {
-        "userform":userform,
+        # "userform":userform,
         "courseform":courseform,
         "sectionform":sectionform,
         "lectureform": lectureform,
@@ -96,15 +102,19 @@ def login_view(request):
         # Attempt to sign user in
         email = request.POST["email"]
         password = request.POST["password"]
+        # username = 'bhainskitaang@gmail.com'
+        # password = '12345678910'
         user = authenticate(request, username=email, password=password)
-
+        # user = authenticate(request, username=bhainskitaang@gmail.com, password=12345678910)
         # Check if authentication successful
         if user is not None:
             login(request, user)
             return HttpResponseRedirect(reverse("index"))
         else:
             return render(request, "learn/login.html", {
-                "message": "Invalid email and/or password."
+                "message": "Invalid email and/or password.",
+                "username":email,
+                "password":password,
             })
     else:
         return render(request, "learn/login.html")
